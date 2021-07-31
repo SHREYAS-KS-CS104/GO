f0da1f2666207b665e089fd980d98433eeec3f00
Shreyas <shreyasks.cs18@bmsce.ac.in>
2021-07-31 21:01:12 +0530

    .

---
+++ Misc/typeCast.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Int go we can't declare a float64 value to int variable
+	length := 100
+	breath := 25.12
+	fmt.Println("The area of the rectangle is: ", length*int(breath))
+	//When we type cast a float64 to a int, it's decimal portion is truncated
+	fmt.Println("The actual area of the rectangle is: ", float64(length)*breath)
+	//Whenever we are type casting we have to close attention to which variable we want to type cast
+}
