1aadf0e2eed1c6b48a41f8068dda6e66857aa4ad
Shreyas <shreyasks.cs18@bmsce.ac.in>
2021-07-31 22:44:12 +0530

    .

---
+++ Misc/reInforce1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The length of the name is: ", len(os.Args[1]))
+	color := "green"
+	color = "Dark " + "bule"
+	n := 0.
+	pi := 3.14
+	num := 2
+	n = float64(num) * pi
+	fmt.Println("The value of n is: ", n)
+	fmt.Println("The color is: ", color)
+	width, length := 6, 5
+	fmt.Println("The perimeter of the rectangle is: ", 2*(length+width))
+}
