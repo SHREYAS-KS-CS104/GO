5a81fa60f6d97bc79554add91de633e3f9147cfd
Shreyas <shreyasks.cs18@bmsce.ac.in>
2021-08-01 17:08:49 +0530

    .

--- Banger/banger.go
+++ Banger/banger.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	var s string
-	s = strings.ToUpper(os.Args[1]) + strings.Repeat("!", len(os.Args[1]))
+	s =  strings.Reapeat("!",len(os.Args[1]) + strings.ToUpper(os.Args[1]) + strings.Repeat("!", len(os.Args[1]))
 	//This program takes an input from the terminal and captitalizes it and add exclamation marks
 	fmt.Println(s)
 }
