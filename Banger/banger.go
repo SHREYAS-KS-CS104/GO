f23088200b60a4ab4837a76559c3852d23249caf
Shreyas <shreyasks.cs18@bmsce.ac.in>
2021-08-01 17:05:31 +0530

    .

---
+++ Banger/banger.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var s string
+	s = strings.ToUpper(os.Args[1]) + strings.Repeat("!", len(os.Args[1]))
+	//This program takes an input from the terminal and captitalizes it and add exclamation marks
+	fmt.Println(s)
+}
